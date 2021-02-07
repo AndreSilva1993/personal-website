fd68f2aebb2a4862e2fa9d1375be4c09b231e298
André Silva <andre.silva@burberry.com>
2021-02-07 20:16:48 +0000

    Fixes font not being properly applied

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ class AppDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href={`https://fonts.googleapis.com/css2?family=Poppins:wght@${requiredFontWeights}&display=swap`}
+            href={`https://fonts.googleapis.com/css2?family=Poppins:wght@${requiredFontWeights}`}
             rel="stylesheet"
           />
         </Head>
